476d77dcb740831d645f7c35864fa6eaf629c548
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2025-05-21 16:34:29 +0900

    chore: chakra ui setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
 import "@/css/index.css"
 import App from "@/pages/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 );
