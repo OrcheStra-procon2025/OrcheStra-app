e0c0d641bd105f3d4afea2a8482ef9d41624eb85
okaits <okaits@okaits7534.net>
2025-08-11 21:47:14 +0900

    feat: 選択UI作成（プロトタイプ）

---
+++ src/utils/selectableMusic.ts
@@ -0,0 +1,13 @@
+export const selectableMusic = [
+  { id: 1, title: "Song A", artist: "Artist A" },
+  { id: 2, title: "Song B", artist: "Artist B" },
+  { id: 3, title: "Song C", artist: "Artist C" },
+  { id: 4, title: "Song D", artist: "Artist D" },
+  { id: 5, title: "Song E", artist: "Artist E" },
+  { id: 6, title: "Song F", artist: "Artist F" },
+  { id: 7, title: "Song G", artist: "Artist G" },
+  { id: 8, title: "Song H", artist: "Artist H" },
+  { id: 9, title: "Song I", artist: "Artist I" },
+  { id: 10, title: "Song J", artist: "Artist J" },
+  { id: 11, title: "Song K", artist: "Artist K" },
+];
