caa2a6bedda95306f5d1e4077e06ce336c828ae8
nos-ice <so20090701@gmail.com>
2025-09-25 19:42:34 +0900

    refac: フォーマッタとリンタを使用

--- src/components/threejs/ThreejsEffect.tsx
+++ src/components/threejs/ThreejsEffect.tsx
@@ -17,18 +17,18 @@ const rand = (min: number, max: number): number => {
 
 const Model = ({ id, position, url, onDelete }: Props) => {
   const { scene } = useGLTF(url);
-  const randomPosition = useRef<[number, number, number]>([0, 0, 0])
-  const createdAtRef = useRef<number>(0)
-  const clonedSceneRef = useRef<Object3D>(new Object3D)
-  
+  const randomPosition = useRef<[number, number, number]>([0, 0, 0]);
+  const createdAtRef = useRef<number>(0);
+  const clonedSceneRef = useRef<Object3D>(new Object3D());
+
   const defaultRotateY = useRef<number>(0);
   const timeToDelete = useRef<number>(0);
   const directionRef = useRef<number>(0);
   const directionPowerRef = useRef<number>(0);
   const rotateDirectionRef = useRef<number>(0);
   const randomScaleRef = useRef<number>(0);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     randomPosition.current = position;
     randomPosition.current[0] += rand(-0.5, 0.5);
     randomPosition.current[1] += rand(-0.5, 0.5);
@@ -42,8 +42,9 @@ const Model = ({ id, position, url, onDelete }: Props) => {
     directionRef.current = Math.random() <= 0.5 ? 1 : -1;
     directionPowerRef.current = rand(-1, 1);
     rotateDirectionRef.current = Math.random() <= 0.5 ? 1 : -1;
-    randomScaleRef.current = Math.random() <= 0.1 ? rand(1, 1.25) : rand(0.25, 0.5);
-  }, [])
+    randomScaleRef.current =
+      Math.random() <= 0.1 ? rand(1, 1.25) : rand(0.25, 0.5);
+  }, []);
 
   useFrame(() => {
     if (performance.now() - createdAtRef.current > timeToDelete.current) {
@@ -91,7 +92,7 @@ const ThreejsEffect = ({ x, y }: { x: number; y: number }) => {
         position: [x / 100, -y / 100, 0],
       },
     ]);
-    return
+    return;
   }, [x, y]);
 
   const handleDelete = (id: number) => {
