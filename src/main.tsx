059f4b97d8db9e447b28b0e9a2dfeb9141d82d13
dog1nu <32312@toyota.kosen-ac.jp>
2025-09-29 00:34:56 +0900

    存在しないページをいったん削除

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "@/css/index.css";
 import SelectionPage from "./pages/SelectionPage";
 import PlayingPage from "./pages/PlayingPage";
-import Result from "./pages/Result";
+//import Result from "./pages/Result";
 //import Result from "./pages/result";
 
 createRoot(document.getElementById("root")!).render(
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<SelectionPage />} />
           <Route path="play" element={<PlayingPage />} />
-          <Route path="result" element={<Result/>} />
+         
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
